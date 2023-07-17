d0cdbf09627cb9a42b3e9840df9230a11f1ea523
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2023-07-17 02:51:10 +0200

    Created custom user model

--- KartozaPortfolio/chuckycharles/admin.py
+++ KartozaPortfolio/chuckycharles/admin.py
@@ -6,6 +6,7 @@ from django.forms import TextInput, Textarea
 class UserAdminConfig(UserAdmin):
     ordering = ['user_name']
     list_display = ('email','user_name','first_name', 'last_name', 'address', 'is_active', 'is_staff')
+    search_fields = ('email','user_name','first_name', 'last_name')
     
 # Register your models here.
 admin.site.register(Profile, UserAdminConfig)
