2ffd6cf840fe0b3e4ad348d4026ea758dee32c1d
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2023-07-17 15:11:27 +0200

    added a home page requesting a form for signing up

--- KartozaPortfolio/chuckycharles/views.py
+++ KartozaPortfolio/chuckycharles/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from chuckycharles.models import Profile, CustomAccountManager
+from django import forms
+from chuckycharles.forms import ProfileForm
 
 # Create your views here.
 from django.http import HttpResponse
+
+
+def home(request):
+    form = ProfileForm()
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'home.html', context)
+
+def login(request):
+    return render(request, "login.html")
 
 
 def index(request):
