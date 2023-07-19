96ea2d550dbd1f564003e4ca325c8135f7899bb4
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2023-07-19 12:08:49 +0200

    Added dashboard, profile, allusers, login and out

--- KartozaPortfolio/chuckycharles/urls.py
+++ KartozaPortfolio/chuckycharles/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
+    path("", views.home, name="Register"),
+    path("login/", views.loginPage, name="login"),
+    path("logout/", views.logoutUser, name="logout"),
     path("index/", views.index, name="index"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("profile/", views.profileuser, name="profile"),
+    path("allusers", views.allusers, name="profile"),
 ]
