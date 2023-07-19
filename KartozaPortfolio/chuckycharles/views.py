973b156497e5fa297404d9f7f5918e5446209f4d
Mudima, TC, Mr [23969156@sun.ac.za] <23969156@sun.ac.za>
2023-07-19 12:09:52 +0200

    Added login page request, authentication of user

--- KartozaPortfolio/chuckycharles/views.py
+++ KartozaPortfolio/chuckycharles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from chuckycharles.models import Profile, CustomAccountManager
 from django import forms
@@ -6,22 +6,62 @@ from chuckycharles.forms import ProfileForm
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     form = ProfileForm()
-
     if request.method == 'POST':
         form = ProfileForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect('home')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(request, email=email, password=password)
+            user = form.cleaned_data.get('user_name')
+            messages.success(request, "Account was created for " + user + " successfully!")
+            return redirect('login')
     context = {'form': form}
     return render(request, 'home.html', context)
 
-def login(request):
-    return render(request, "login.html")
+def loginPage(request):
+    
+    # checking if user details are correct
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
 
+        # if user details are correct, then login (if user is actually there)
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Login successful!")
+            return redirect('dashboard') # redirect to home or index page
+        else:
+            messages.info(request, "Username or Password is incorrect!")
+            return render(request, "login.html")
+    context = {}
+    return render(request, "login.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+def dashboard(request):
+    profile = request.POST.get('user_name')
+    
+    context = {}
+    
+    return render(request, "dashboard.html", context)
+
+def profileuser(request):
+    context = {}
+    return render(request, "profile.html", context)
+
+def allusers(request):
+    context = {}
+    location = request.POST.get('location')
+    return render(request, "all_users.html", context)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
